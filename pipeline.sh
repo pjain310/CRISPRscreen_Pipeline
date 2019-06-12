26c638ee927968226cc0b048ca08dd4be36c781a
pjain310 <08.prerna@gmail.com>
2019-06-12 17:16:51 -0400

    Added fdr correction for p value

--- pipeline.sh
+++ pipeline.sh
@@ -60,7 +60,7 @@ prepareDirectory() {
   		echo "directory ${dest} already exists, would you like to overwrite? Reply with y/n"
   		read answer
   		case $answer in
-  			y) echo "Overwriting temp diectory"
+  			y) echo "Overwriting ${dest} diectory"
            mkdir -p ${dest};;
   			n) echo "Folder overwrite denied, exiting pipeline"
   				 exit 1;;
@@ -85,7 +85,7 @@ get_input() {
   while getopts "i:s:o:t:vh" option; do
     case $option in
       i) counts=$OPTARG;;
-      s) sample_map=$OPTARG;;
+      s) sample_mapf=$OPTARG;;
       o) output_dir=$OPTARG;;
       t) tools+=($OPTARG);;
       v) v=1;;
@@ -98,19 +98,19 @@ get_input() {
   done
 
   #Check for presence of required arguments (counts file, output directory, sample map file)
-  declare -a required_opts=("counts" "output_dir" "sample_map")
-  for opt in "${required_opts[@]}"; do 
-    if [[ -z ${!opt} ]]; then 
-      echo "Missing required option \"$opt\", exiting..." 
+  declare -a required_opts=("counts" "output_dir" "sample_mapf")
+  for opt in "${required_opts[@]}"; do
+    if [[ -z ${!opt} ]]; then
+      echo "Missing required option \"$opt\", exiting..."
       exit $LINENO
     fi
   done
 
   #Check if output directory is already present. If present, give option to rewrite.
-  if ! prepareDirectory $output_dir; then 
+  if ! prepareDirectory $output_dir; then
     exit $LINENO
   fi
-  
+
   #Check for counts file
   if [ ! -f $counts ]
   then
@@ -119,7 +119,7 @@ get_input() {
   fi
 
   #Check for sample map file
-  if [ ! -f $sample_map ]
+  if [ ! -f $sample_mapf ]
   then
     echo "Path to sample map file incorrect or file missing. Exiting pipeline"
     exit $LINENO
@@ -127,7 +127,7 @@ get_input() {
 
   #Export variables so that they can be used within xargs
   export counts
-  export sample_map
+  export sample_mapf
 
 }
 
@@ -136,7 +136,7 @@ convert_output(){
   echo "Op converter"
 }
 
-run_mageck(){
+run_mageckrra(){
   #Description: First, use sample_mapper functions to convert sample map into appropriate format for software.
   #             Second, Run MAGeCK-RRA on counts data.
   #             Third convert output to consistent format using convert_output()
@@ -145,7 +145,9 @@ run_mageck(){
   mkdir -p temp/mageckrra
 
   #mageck sampler prints appropriate args from the sample_map to be fed to mageck test
-  python -c "from python_modules.sample_mapper import *; mageck_sampler(makeSampleMap('$sample_map'))" | xargs -n3 bash -c 'mageck test -k $counts -t $1 -t $2 -n temp/mageckrra/"$0"_vs_"$2"'
+  python -c "from python_modules.sample_mapper import *; mageckrra=mageckrra('$sample_mapf',''); mageckrra.makeSampleMapDict(); mageckrra.SampleMapper()" | xargs -n4 bash -c "mageck test --adjust_method fdr -k $counts -t \$2 -c \$3 -n temp/mageckrra/\$0_vs_\$1"
+
+  mv temp/mageckrra $output_dir/
 
 }
 
@@ -157,16 +159,36 @@ run_mageck_mle(){
 run_pbnpa(){
   #Description: Run pbnpa on counts data, convert output to consistent format using convert_output()
   echo "Running pbnpa"
+
+  mkdir -p temp/pbnpa
+
+  python -c "from python_modules.sample_mapper import *; pbnpa=pbnpa('$sample_mapf',''); pbnpa.makeSampleMapDict(); pbnpa.SampleMapper()" | xargs -n1 bash -c "Rscript ./PBNPA.R \$counts \$0"
+
+  mv temp/pbnpa $output_dir/
 }
 
 run_cb2(){
   #Description: Run cb2 on counts data, convert output to consistent format using convert_output()
   echo "Running cb2"
+
+  mkdir -p temp/cb2
+
+  #Run cb2 sampler and cb2 R script
+  python -c "from python_modules.sample_mapper import *; cb2=cb2('$sample_mapf',''); cb2.makeSampleMapDict(); cb2.SampleMapper()" | xargs -n1 bash -c "Rscript ./CB2.R \$counts \$0"
+
+  mv temp/cb2 $output_dir/
 }
 
 run_bagel(){
   #Description: Run bagel on counts data, convert output to consistent format using convert_output()
   echo "Running bagel"
+
+  mkdir -p temp/bagel
+
+  #Run bagel sampler and bagel scripts
+  python -c "from python_modules.sample_mapper import *; bagel=bagel('$sample_mapf','','$counts'); bagel.makeSampleMapDict(); bagel.SampleMapper()" | xargs -n3 bash -c "BAGEL-calc_foldchange.py -i \$counts -o temp/bagel/\$0 -c \$1; BAGEL.py -i temp/bagel/\$0.foldchange -o temp/bagel/\$0_bagel -e training_essentials.txt -n training_nonessential.txt -c \$2"
+
+  mv temp/bagel $output_dir/
 }
 
 
@@ -178,22 +200,22 @@ main() {
 	get_input "$@"
 
   #Create temp directory
-  if ! prepareDirectory "temp"; then 
+  if ! prepareDirectory "temp"; then
     exit $LINENO
-  fi 
+  fi
 
   #Run tools according to option specified in tools flag
-  if [[ -z $tools ]]; then 
-    tools=("mageck" "mageck_mle" "pbnpa" "cb2" "bagel")
+  if [[ -z $tools ]]; then
+    tools=("mageckrra" "mageck_mle" "pbnpa" "cb2" "bagel")
   fi
 
   for tool in "${tools[@]}"; do
-    if ! run_${tool} ; then 
+    if ! run_${tool} ; then
       echo "Failed to run tool \"${tool}\"..."
     fi
   done
 
-  #rm -r temp
+  rm -r temp
 
 }
 
